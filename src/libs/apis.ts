8f5b0ca90dd3a4525997ddb97a8a19f1bc34f7fc
Juan Castro <jnxcastro@gmail.com>
2024-08-26 12:19:10 -0500

    feat: featured room

---
+++ src/libs/apis.ts
@@ -0,0 +1,14 @@
+import { Room } from "@/app/models/room";
+import sanityClient from "./sanity";
+import * as queries from "./sanityQueries";
+
+export async function getFeaturedRoom() {
+    const result = await sanityClient.fetch<Room>(
+        queries.getFeaturedRoomQuery, 
+        {},
+        { cache: 'no-cache'}
+        // { next: {revalidate:1800}} // 30 min
+    );
+
+    return result;
+}
